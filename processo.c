2373246dc9994ea5fa92fc729a94289c7f095c33
Guilherme-Schwann <guilhermeawilke@hotmail.com>
2021-12-11 19:36:04 -0400

    Update processo.c

--- processo.c
+++ processo.c
@@ -9,11 +9,15 @@ TProcesso inicializaProcesso() {
     time_t instante = time(NULL);
     processo.horaCriada = localtime(&instante);
     srand(time(0));
+<<<<<<< Updated upstream
 <<<<<<< HEAD
     //cria ID aleatório de 4  dígitos :
 =======
     //cria ID aleatório de 4 dígitos :
 >>>>>>> e4f34ce4bfc34a1848acd6bd3ed45e275cdcc923
+=======
+    //cria ID aleatório de 4 dígitos:
+>>>>>>> Stashed changes
     processo.pId = (rand() % 9000) + 1000;
     //cria prioridade aleatória de 1 a 5:
     processo.prior = (rand() % 5) + 1;
