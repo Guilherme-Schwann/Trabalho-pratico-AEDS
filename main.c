0c77492f0a2d812ad771bda732d1a6a671d85e44
Letícia Silva <95985205+lleticiasilvaa@users.noreply.github.com>
2021-12-15 08:22:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+//para testar a implementaC'C#o dos seu TAD, voce devera implementar um 
+//programa principal que simula a gerencia de uma lista de processos feita pelo sistema operacional 
+//este deve possuir as funcionalidade de:
+//- Inicializacao da lista de processos;
+//- Insercao de novos processos na lista;
+//- Remocao de processos da lista (primeiro);
+//- Impressao da lista (na saida padrao);
+
+//devera permitir a criaca interativa de uma lista, bem como o carregamento de uma nova lista a partir de um arquivo 
+//o tamanho da lista e, portanto, definido pelo usuario
+//em seguida, devera permitir que o usuario indique o numero de insercoes ou remocoes que deseja realizar sobre a lista 
+//ou le essas informacoes do arquivo (?) efetuando tais operacoes
+//por fim, o seu sistema de testes devera medir qual o tempo gasto para a realizacao de cada teste.
+
+//arquivo entrada
+//devera possuir o seguinte formato:
+//N // Tamanho do vetor para cursores
+//NLO // NC:mero de linhas de operacoes especificadas abaixo
+//Op Qt // Op = 0 para insercao e 1 para remocao inicio
+	// Qt = quantidade de vezes que a operacao sera realizada
+
+//arquivo saida
+//devera ser uma tabela constando o numero do teste e o tempo total de execucaoo encontrado para sua realizacao.
+//NUM_TESTE <tempo> // NC:mero do teste, valor para TAD cursores(?)
+
+
 #include <stdio.h>
 #include "processo.h"
 
